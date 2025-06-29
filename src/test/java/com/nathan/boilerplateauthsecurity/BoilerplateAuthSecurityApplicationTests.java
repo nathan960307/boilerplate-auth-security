c786991e968fbf4dbf1985497e05898c80178b54
nathan <nathan960307@gmail.com>
2025-06-29 19:46:17 +0900

    init: #1 프로젝트 기본 세팅 및 .gitignore 추가

---
+++ src/test/java/com/nathan/boilerplateauthsecurity/BoilerplateAuthSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.boilerplateauthsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateAuthSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
