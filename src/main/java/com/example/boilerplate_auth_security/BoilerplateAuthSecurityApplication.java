759ceb9c1413f53754e99456d6bc68910673275e
nathan <nathan960307@gmail.com>
2025-08-05 20:52:37 +0900

    (0805) feat: #1 Spring boot 인증 서버 기본 구조 세팅(2252)

---
+++ src/main/java/com/example/boilerplate_auth_security/BoilerplateAuthSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.boilerplate_auth_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateAuthSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerplateAuthSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/boilerplate_auth_security/BoilerplateAuthSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boilerplate_auth_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateAuthSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
