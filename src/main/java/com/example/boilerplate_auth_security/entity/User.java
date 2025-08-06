6e8f64e879eb25679a71776d1c57ec5f94982871
nathan <nathan960307@gmail.com>
2025-08-06 15:47:25 +0900

    (0806) feat: #2 user entity 추가, UserRepository추가, jpa 의존성 추가(1546)

---
+++ src/main/java/com/example/boilerplate_auth_security/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.boilerplate_auth_security.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String role;
+}
---
+++ src/main/java/com/example/boilerplate_auth_security/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.boilerplate_auth_security.repository;
+
+import com.example.boilerplate_auth_security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    boolean existsByEmail(String email); // 이메일 중복 체크
+    Optional<User> findByEmail(String email);
+
+}
